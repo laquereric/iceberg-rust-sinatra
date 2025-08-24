376dda88ff2a230236d9d17c1cf64964d3e31be9
Eric Laquer <LaquerEric@gmail.com>
2025-08-23 20:03:41 -0400

    ext builds

--- ext/iceberg_rust_sinatra/src/lib.rs
+++ ext/iceberg_rust_sinatra/src/lib.rs
@@ -1,13 +1,50 @@
-use rutie::{Class, Object, RString, VM};
+use rutie::{AnyObject, Class, Object, RString, VM};
+use iceberg_rust::catalog::Catalog;
+use std::collections::HashMap;
+use anyhow::Result;
+use tokio::runtime::Runtime;
+
+#[derive(Debug)]
+struct IcebergCatalog {
+    catalog: Catalog,
+}
+
+impl IcebergCatalog {
+    fn new(config: HashMap<String, String>) -> Result<Self> {
+        let catalog = Catalog::new(&config)?;
+        Ok(IcebergCatalog { catalog })
+    }
+
+    fn create_namespace(&self, namespace: &str) -> Result<()> {
+        self.catalog.create_namespace(namespace, None)?;
+        Ok(())
+    }
+}
+
+rutie::class!(RubyCatalog);
+
+methods!(
+    RubyCatalog,
+    rtself,
+    fn ruby_catalog_new(config: AnyObject) -> AnyObject {
+        let config_hash: HashMap<String, String> = config.try_into().unwrap();
+        let catalog = IcebergCatalog::new(config_hash).unwrap();
+        Class::from_existing("RubyCatalog").wrap_data(catalog, &*RACK_CATALOG)
+    },
+    fn ruby_catalog_create_namespace(namespace: RString) -> RString {
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async {
+            let catalog = rtself.get_data(&*RACK_CATALOG);
+            catalog.create_namespace(&namespace.to_string()).unwrap();
+        });
+        namespace
+    }
+);
 
 #[allow(non_snake_case)]
 #[no_mangle]
 pub extern "C" fn Init_iceberg_rust_sinatra() {
-    Class::new("IcebergRustSinatra", None).define(|klass| {
-        klass.def_self("hello", hello);
-    });
-}
-
-fn hello(_: &Class) -> RString {
-    RString::new_utf8("Hello from Rust!")
+    let mut class = Class::new("RubyCatalog", None);
+    class.def_self("new", ruby_catalog_new);
+    class.def("create_namespace", ruby_catalog_create_namespace);
 }
--- lib/iceberg/rust/sinatra.rb
+++ lib/iceberg/rust/sinatra.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 require_relative "sinatra/version"
+require "rutie"
 
 module Iceberg
   module Rust
     module Sinatra
       class Error < StandardError; end
-      # Your code goes here...
+      
+      Rutie.new(:iceberg_rust_sinatra).init "Init_iceberg_rust_sinatra", __dir__
     end
   end
 end
